30988ad885150788afdb20aa873bf72adbeaa779
alirahmanicode <alirahmanicode@gmail.com>
2024-04-13 12:15:16 +0330

    initial server

---
+++ src/api/api.go
@@ -0,0 +1,13 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func InitServer() {
+	r := gin.New()
+	r.Use(gin.Logger(), gin.Recovery())
+	
+	r.Run(":5000")
+
+}
---
+++ src/cmd/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/alirahmanicode/golang-web-api/api"
+
+
+
+func main() {
+	api.InitServer()
+}
